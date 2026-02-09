73981d43f641d72151b60a8c4a4bfee4e82809f7
Stanislav Muzulev <muz-stanislav@yandex.ru>
2026-02-09 21:05:59 +0300

    add validation for Post requests

--- src/main/java/com/post_hub/iam_service/controller/PostController.java
+++ src/main/java/com/post_hub/iam_service/controller/PostController.java
@@ -8,6 +8,7 @@ import com.post_hub.iam_service.model.request.post.PostRequest;
 import com.post_hub.iam_service.model.response.IamResponse;
 import com.post_hub.iam_service.service.PostService;
 import com.post_hub.iam_service.utils.ApiUtils;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -32,7 +33,7 @@ public class PostController {
 
     @PostMapping("${end.point.create}")
     public ResponseEntity<IamResponse<PostDto>> createPost(
-            @RequestBody PostRequest postRequest) {
+            @RequestBody @Valid PostRequest postRequest) {
         log.trace(ApiLogMessage.NAME_OF_CURRENT_METHOD.getValue(), ApiUtils.getMethodName());
 
         IamResponse<PostDto> response = postService.createPost(postRequest);
--- src/main/java/com/post_hub/iam_service/service/Impl/PostServiceImpl.java
+++ src/main/java/com/post_hub/iam_service/service/Impl/PostServiceImpl.java
@@ -4,6 +4,7 @@ import com.post_hub.iam_service.mapper.PostMapper;
 import com.post_hub.iam_service.model.constants.ApiErrorMessage;
 import com.post_hub.iam_service.model.dto.Post.PostDto;
 import com.post_hub.iam_service.model.entity.Post;
+import com.post_hub.iam_service.model.exception.DataExistException;
 import com.post_hub.iam_service.model.exception.NotFoundException;
 import com.post_hub.iam_service.model.request.post.PostRequest;
 import com.post_hub.iam_service.model.response.IamResponse;
@@ -32,10 +33,13 @@ public class PostServiceImpl implements PostService {
 
     @Override
     public IamResponse<PostDto> createPost(@NotNull PostRequest postRequest) {
+
+        if (postRepository.existsByTitle(postRequest.getTitle())) {
+            throw new DataExistException(ApiErrorMessage.POST_ALREADY_EXISTS.getMessage(postRequest.getTitle()));
+        }
+
         Post post = postMapper.createPost(postRequest);
-
         Post savedPost = postRepository.save(post);
-
         PostDto postDto = postMapper.toPostDto(savedPost);
 
         return IamResponse.createSuccessful(postDto);
